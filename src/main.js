d9883fbf2c8bc3ceb20209eff78b38c1410de030
sanoani <naizou.horumon@gmail.com>
2015-12-04 16:10:30 +0900

    Add Pepper Flash plugin

--- src/main.js
+++ src/main.js
@@ -20,6 +20,11 @@ app.on('window-all-closed', function () {
   }
 });
 
+app.commandLine.appendSwitch('ppapi-flash-path', __dirname + '/PepperFlash/PepperFlashPlayer.plugin');
+
+// Specify flash version, for example, v18.0.0.203
+app.commandLine.appendSwitch('ppapi-flash-version', '19.0.0.245');
+
 // Electronの初期化完了後に実行
 app.on('ready', function () {
   mainWindow = new BrowserWindow({
@@ -29,7 +34,7 @@ app.on('ready', function () {
       'plugins': true
     }
   });
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // ウィンドウが閉じられたらアプリも終了
   mainWindow.on('closed', function () {
