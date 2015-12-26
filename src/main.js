2c2bf4c6cff5ecb135fa4592068138f415552be1
sanoani <naizou.horumon@gmail.com>
2015-12-26 11:09:00 +0900

    add photon

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ app.on('window-all-closed', function () {
     app.quit();
 });
 
-// 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function () {
   if (process.platform != 'darwin') {
     app.quit();
@@ -21,22 +20,17 @@ app.on('window-all-closed', function () {
 });
 
 app.commandLine.appendSwitch('ppapi-flash-path', __dirname + '/PepperFlash/PepperFlashPlayer.plugin');
-
-// Specify flash version, for example, v18.0.0.203
 app.commandLine.appendSwitch('ppapi-flash-version', '19.0.0.245');
 
-// Electronの初期化完了後に実行
 app.on('ready', function () {
   mainWindow = new BrowserWindow({
     width: 960,
-    height: 580,
+    height: 980,
     'web-preferences': {
       'plugins': true
     }
   });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // ウィンドウが閉じられたらアプリも終了
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
--- src/tohlove.js
+++ src/tohlove.js
@@ -1,11 +1,25 @@
-'use strict';
-var remote = require('remote');
-var app = remote.require('app');
+webview.executeJavaScript("document.write('JavaScriptをWebview内で実行することができます。')");
 
-var webview = document.getElementById('mainWebView');
-var title = '';
+$(function () {
+  $('#loadButton').click(function () {
+    //alert('ほげ');
+    //$('#result').load('more.html');
+    $.ajax({
+      type: 'GET',
+      //url: 'http://iwb.jp/s/js/data.json',
+      //url: './tohlove_json/list.json',
+      //url: 'http://203.104.209.102/kcsapi/api_get_member/ndock',
+      cache: true,
+      datatype: 'json',
+      success: function (json) {
+        $.each(json, function (i, item) {
+          $('body').prepend(item.codename + '<br>');
+        });
+      },
+      error: function () {
+        alert('error');
+      }
+    });
+  });
 
-// check title every second
-setInterval(function () {
-  title = webview.getTitle();
-}, 1000);
+});
